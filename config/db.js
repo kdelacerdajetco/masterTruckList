7a11014ce4b67416f94ae7e780bdee2bd023026e
Kristen De La Cerda <kdelacerda@jetcodelivery.com>
2018-09-27 16:30:32 -0500

    null update

--- config/db.js
+++ config/db.js
@@ -1,11 +1,17 @@
 // Set the connection string based from the config vars of the production server
 // To run locally use 'mongodb://localhost/mern-crud' instead of process.env.DB
+//  var db;
 
 module.exports = {
  db: process.env.DB
 //  db: 'mongodb://localhost/mastertrucklist'
 };
 
+// mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/test", function (err, client) {
+//   if (err) {
+//     console.log(err);
+//     process.exit(1);
+//   }
 
 // {
 //     "development": {
