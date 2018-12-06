69c76db97a339bd280bf638841d33614ae628c69
kdelacerdajetco <kdelacerda@jetodelivery.com>
2018-12-06 14:18:13 -0600

    try 1

--- react-src/src/components/App/App.js
+++ react-src/src/components/App/App.js
@@ -47,8 +47,14 @@ class App extends Component {
     this.socket.on('delete', data => this.handleUserDeleted(data));
 
     // might break below 12.06.2018
-    const users = this.state.users.slice();
-    this.setState({ users: users, allUsers: users})
+    // const users = this.state.users.slice();
+        // const users = this.socket.on('filter', data => this.setState({ users: data, allUsers: data}));
+        // const users = this.socket.on('filter', data => this.setState(data));
+        // const users = this.socket.on('filter', data => this.searchUsers({users: data, allUsers: data}));
+
+    const users = this.socket.on('filter', data => this.searchUsers(data));
+    this.setState({ users: users, allUsers: users })
+    
 
     // 12.06.2018
     // this.socket.on('filter', data => this.searchUsers(data));
